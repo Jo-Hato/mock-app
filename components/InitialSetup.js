ff821b589270c255e2611722d7741374c7f0ea22
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-07-19 14:29:44 +0900

    Made the initial setup/debug page. TODO: Add a button for proceeding to the next eventNum

--- components/InitialSetup.js
+++ components/InitialSetup.js
@@ -1,8 +1,28 @@
 app.component('initial-setup', {
+    props: {
+        accels: {
+          type: Array,
+          required: true
+        },
+        gyros: {
+            type: Array,
+            required: true
+          },
+      },
     template:
     /*html*/
     `<h1>Initial Setup</h1>
-    
+
+    <p>Accelerometers:<br>
+        X: {{ accels[0] }}<br>
+        Y: {{ accels[1] }}<br>
+        Z: {{ accels[2] }}<br>
+    </p>
+    <p>Gyrometers:<br>
+        X: {{ gyros[0] }}<br>
+        Y: {{ gyros[1] }}<br>
+        Z: {{ gyros[2] }}<br>
+    </p>
     `,
     beforeMount(){
       }
