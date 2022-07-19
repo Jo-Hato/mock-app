90c13ffcc0085453fc1d34f546c16da760bf4701
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-07-20 08:35:10 +0900

    Trying out different device snesor API since it doesn't work on iOS devices, plz work...

--- main.js
+++ main.js
@@ -13,39 +13,5 @@ const app = Vue.createApp({
             this.experimentData.push(initialForm)
             this.eventNum++
         },
-        /*handleOrientation(event) {
-            updateFieldIfNotNull('Orientation_a', event.alpha);
-            updateFieldIfNotNull('Orientation_b', event.beta);
-            updateFieldIfNotNull('Orientation_g', event.gamma);
-            incrementEventCount();
-        },
-          
-        incrementEventCount(){
-            let counterElement = document.getElementById("num-observed-events")
-            let eventCount = parseInt(counterElement.innerHTML)
-            counterElement.innerHTML = eventCount + 1;
-        },
-          
-        updateFieldIfNotNull(fieldName, value, precision=10){
-            if (value != null)
-                document.getElementById(fieldName).innerHTML = value.toFixed(precision);
-        },
-          
-        handleMotion(event) {
-            updateFieldIfNotNull('Accelerometer_gx', event.accelerationIncludingGravity.x);
-            updateFieldIfNotNull('Accelerometer_gy', event.accelerationIncludingGravity.y);
-            updateFieldIfNotNull('Accelerometer_gz', event.accelerationIncludingGravity.z);
-          
-            updateFieldIfNotNull('Accelerometer_x', event.acceleration.x);
-            updateFieldIfNotNull('Accelerometer_y', event.acceleration.y);
-            updateFieldIfNotNull('Accelerometer_z', event.acceleration.z);
-          
-            updateFieldIfNotNull('Accelerometer_i', event.interval, 2);
-          
-            updateFieldIfNotNull('Gyroscope_z', event.rotationRate.alpha);
-            updateFieldIfNotNull('Gyroscope_x', event.rotationRate.beta);
-            updateFieldIfNotNull('Gyroscope_y', event.rotationRate.gamma);
-            incrementEventCount();
-        }*/
     }
 })
