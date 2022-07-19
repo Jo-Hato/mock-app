c2650f78a19ab741cb6814d2f12aab1b991f0d91
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-07-20 08:48:58 +0900

    It's a good practice for iOS for having more security for device sensors, but this sucks...

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ const app = Vue.createApp({
             SENSORFREQ: 20,
             accels: [0, 0, 0],
             gyros: [0, 0, 0],
+            
+            //is_running : false,
+            //demo_button : document.getElementById("start_demo"),
         }
     },
     methods: {
