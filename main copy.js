d893996807456c5c553180f56219608151ee8ac7
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-07-19 16:41:13 +0900

    Trying out different device snesor API since it doesn't work on iOS devices, plz work...

---
+++ main copy.js
@@ -0,0 +1,64 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            eventNum: 0,
+            experimentData: [],
+            SENSORFREQ: 20,
+            accels: [0, 0, 0],
+            gyros: [0, 0, 0],
+        }
+    },
+    methods: {
+        addInitialForm(initialForm) {
+            this.experimentData.push(initialForm)
+            this.eventNum++
+        },
+    },
+    beforeMount(){
+
+        /*
+        navigator.permissions.query({ name: 'accelerometer' })
+        .then(result => {
+            if (result.state === 'denied') {
+                console.log('Permission to use accelerometer sensor is denied.');
+                return;
+            }
+            navigator.permissions.query({ name: 'accelerometer' })
+            .then(result => {
+            if (result.state === 'denied') {
+                console.log('Permission to use accelerometer sensor is denied.');
+                return;
+            }
+                let acl = new Accelerometer({frequency: this.SENSORFREQ});
+                acl.addEventListener('reading', () => {
+                this.accels[0] = acl.x
+                this.accels[1] = acl.y
+                this.accels[2] = acl.z
+                //console.log("Acceleration along the X-axis " + acl.x);
+                //console.log("Acceleration along the Y-axis " + acl.y);
+                //console.log("Acceleration along the Z-axis " + acl.z);
+                });
+                acl.start();
+            });
+            
+            navigator.permissions.query({ name: 'gyroscope' })
+            .then(result => {
+              if (result.state === 'denied') {
+                console.log('Permission to use gyroscope sensor is denied.');
+                return;
+              }
+              let gyroscope = new Gyroscope({frequency: this.SENSORFREQ});
+              gyroscope.addEventListener('reading', e => {
+              this.gyros[0] = gyroscope.x
+              this.gyros[1] = gyroscope.y
+              this.gyros[2] = gyroscope.z
+              //console.log("Angular velocity along the X-axis " + gyroscope.x);
+              //console.log("Angular velocity along the Y-axis " + gyroscope.y);
+              //console.log("Angular velocity along the Z-axis " + gyroscope.z);
+              });
+              gyroscope.start();
+            });
+        });
+        */
+    }
+})
--- main.js
+++ main.js
@@ -13,96 +13,39 @@ const app = Vue.createApp({
             this.experimentData.push(initialForm)
             this.eventNum++
         },
-        myFunc() {
-            navigator.permissions.query({ name: 'accelerometer' })
-            .then(result => {
-                if (result.state === 'denied') {
-                    console.log('Permission to use accelerometer sensor is denied.');
-                    return;
-                }
-                navigator.permissions.query({ name: 'accelerometer' })
-                .then(result => {
-                if (result.state === 'denied') {
-                    console.log('Permission to use accelerometer sensor is denied.');
-                    return;
-                }
-                    let acl = new Accelerometer({frequency: this.SENSORFREQ});
-                    acl.addEventListener('reading', () => {
-                    this.accels[0] = acl.x
-                    this.accels[1] = acl.y
-                    this.accels[2] = acl.z
-                    //console.log("Acceleration along the X-axis " + acl.x);
-                    //console.log("Acceleration along the Y-axis " + acl.y);
-                    //console.log("Acceleration along the Z-axis " + acl.z);
-                    });
-                    acl.start();
-                });
-                
-                navigator.permissions.query({ name: 'gyroscope' })
-                .then(result => {
-                if (result.state === 'denied') {
-                    console.log('Permission to use gyroscope sensor is denied.');
-                    return;
-                }
-                let gyroscope = new Gyroscope({frequency: this.SENSORFREQ});
-                gyroscope.addEventListener('reading', e => {
-                this.gyros[0] = gyroscope.x
-                this.gyros[1] = gyroscope.y
-                this.gyros[2] = gyroscope.z
-                //console.log("Angular velocity along the X-axis " + gyroscope.x);
-                //console.log("Angular velocity along the Y-axis " + gyroscope.y);
-                //console.log("Angular velocity along the Z-axis " + gyroscope.z);
-                });
-                gyroscope.start();
-                });
-            });
-        }
-    },
-    beforeMount(){
-
-        /*
-        navigator.permissions.query({ name: 'accelerometer' })
-        .then(result => {
-            if (result.state === 'denied') {
-                console.log('Permission to use accelerometer sensor is denied.');
-                return;
-            }
-            navigator.permissions.query({ name: 'accelerometer' })
-            .then(result => {
-            if (result.state === 'denied') {
-                console.log('Permission to use accelerometer sensor is denied.');
-                return;
-            }
-                let acl = new Accelerometer({frequency: this.SENSORFREQ});
-                acl.addEventListener('reading', () => {
-                this.accels[0] = acl.x
-                this.accels[1] = acl.y
-                this.accels[2] = acl.z
-                //console.log("Acceleration along the X-axis " + acl.x);
-                //console.log("Acceleration along the Y-axis " + acl.y);
-                //console.log("Acceleration along the Z-axis " + acl.z);
-                });
-                acl.start();
-            });
-            
-            navigator.permissions.query({ name: 'gyroscope' })
-            .then(result => {
-              if (result.state === 'denied') {
-                console.log('Permission to use gyroscope sensor is denied.');
-                return;
-              }
-              let gyroscope = new Gyroscope({frequency: this.SENSORFREQ});
-              gyroscope.addEventListener('reading', e => {
-              this.gyros[0] = gyroscope.x
-              this.gyros[1] = gyroscope.y
-              this.gyros[2] = gyroscope.z
-              //console.log("Angular velocity along the X-axis " + gyroscope.x);
-              //console.log("Angular velocity along the Y-axis " + gyroscope.y);
-              //console.log("Angular velocity along the Z-axis " + gyroscope.z);
-              });
-              gyroscope.start();
-            });
-        });
-        */
+        /*handleOrientation(event) {
+            updateFieldIfNotNull('Orientation_a', event.alpha);
+            updateFieldIfNotNull('Orientation_b', event.beta);
+            updateFieldIfNotNull('Orientation_g', event.gamma);
+            incrementEventCount();
+        },
+          
+        incrementEventCount(){
+            let counterElement = document.getElementById("num-observed-events")
+            let eventCount = parseInt(counterElement.innerHTML)
+            counterElement.innerHTML = eventCount + 1;
+        },
+          
+        updateFieldIfNotNull(fieldName, value, precision=10){
+            if (value != null)
+                document.getElementById(fieldName).innerHTML = value.toFixed(precision);
+        },
+          
+        handleMotion(event) {
+            updateFieldIfNotNull('Accelerometer_gx', event.accelerationIncludingGravity.x);
+            updateFieldIfNotNull('Accelerometer_gy', event.accelerationIncludingGravity.y);
+            updateFieldIfNotNull('Accelerometer_gz', event.accelerationIncludingGravity.z);
+          
+            updateFieldIfNotNull('Accelerometer_x', event.acceleration.x);
+            updateFieldIfNotNull('Accelerometer_y', event.acceleration.y);
+            updateFieldIfNotNull('Accelerometer_z', event.acceleration.z);
+          
+            updateFieldIfNotNull('Accelerometer_i', event.interval, 2);
+          
+            updateFieldIfNotNull('Gyroscope_z', event.rotationRate.alpha);
+            updateFieldIfNotNull('Gyroscope_x', event.rotationRate.beta);
+            updateFieldIfNotNull('Gyroscope_y', event.rotationRate.gamma);
+            incrementEventCount();
+        }*/
     }
 })
