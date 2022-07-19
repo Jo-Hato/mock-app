593679fbae570dbe7adb56aaac7a1af9cd519760
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-07-19 16:10:18 +0900

    Trying out different device snesor API since it doesn't work on iOS devices, plz work...

--- main.js
+++ main.js
@@ -13,82 +13,49 @@ const app = Vue.createApp({
             this.experimentData.push(initialForm)
             this.eventNum++
         },
-
-        handleOrientationEvent (frontToBack, leftToRight, rotateDegrees) {
-            this.accels[0] = frontToBack
-            this.accels[1] = leftToRight
-            this.accels[2] = rotateDegrees
-        },
-
-        myClick() {
-            // feature detect
-            if (typeof DeviceMotionEvent.requestPermission === 'function') {
-              DeviceMotionEvent.requestPermission()
-                .then(permissionState => {
-                  if (permissionState === 'granted') {
-                    window.addEventListener("deviceorientation", function(event) {
-                        // alpha: rotation around z-axis
-                        const rotateDegrees = event.alpha;
-                        // gamma: left to right
-                        const leftToRight = event.gamma;
-                        // beta: front back motion
-                        const frontToBack = event.beta;
-                
-                        handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
-                    }, true);
-                  }
-                })
-                .catch(console.error);
-            } else {
-              // handle regular non iOS 13+ devices
-            }
-          }
-    },
-    beforeMount(){
-
-        /*
-        navigator.permissions.query({ name: 'accelerometer' })
-        .then(result => {
-            if (result.state === 'denied') {
-                console.log('Permission to use accelerometer sensor is denied.');
-                return;
-            }
+        requestOnClick(){
             navigator.permissions.query({ name: 'accelerometer' })
             .then(result => {
-            if (result.state === 'denied') {
-                console.log('Permission to use accelerometer sensor is denied.');
-                return;
-            }
-                let acl = new Accelerometer({frequency: this.SENSORFREQ});
-                acl.addEventListener('reading', () => {
-                this.accels[0] = acl.x
-                this.accels[1] = acl.y
-                this.accels[2] = acl.z
-                //console.log("Acceleration along the X-axis " + acl.x);
-                //console.log("Acceleration along the Y-axis " + acl.y);
-                //console.log("Acceleration along the Z-axis " + acl.z);
+                if (result.state === 'denied') {
+                    console.log('Permission to use accelerometer sensor is denied.');
+                    return;
+                }
+                navigator.permissions.query({ name: 'accelerometer' })
+                .then(result => {
+                if (result.state === 'denied') {
+                    console.log('Permission to use accelerometer sensor is denied.');
+                    return;
+                }
+                    let acl = new Accelerometer({frequency: this.SENSORFREQ});
+                    acl.addEventListener('reading', () => {
+                    this.accels[0] = acl.x
+                    this.accels[1] = acl.y
+                    this.accels[2] = acl.z
+                    //console.log("Acceleration along the X-axis " + acl.x);
+                    //console.log("Acceleration along the Y-axis " + acl.y);
+                    //console.log("Acceleration along the Z-axis " + acl.z);
+                    });
+                    acl.start();
                 });
-                acl.start();
+                
+                navigator.permissions.query({ name: 'gyroscope' })
+                .then(result => {
+                if (result.state === 'denied') {
+                    console.log('Permission to use gyroscope sensor is denied.');
+                    return;
+                }
+                let gyroscope = new Gyroscope({frequency: this.SENSORFREQ});
+                gyroscope.addEventListener('reading', e => {
+                this.gyros[0] = gyroscope.x
+                this.gyros[1] = gyroscope.y
+                this.gyros[2] = gyroscope.z
+                //console.log("Angular velocity along the X-axis " + gyroscope.x);
+                //console.log("Angular velocity along the Y-axis " + gyroscope.y);
+                //console.log("Angular velocity along the Z-axis " + gyroscope.z);
+                });
+                gyroscope.start();
+                });
             });
-            
-            navigator.permissions.query({ name: 'gyroscope' })
-            .then(result => {
-              if (result.state === 'denied') {
-                console.log('Permission to use gyroscope sensor is denied.');
-                return;
-              }
-              let gyroscope = new Gyroscope({frequency: this.SENSORFREQ});
-              gyroscope.addEventListener('reading', e => {
-              this.gyros[0] = gyroscope.x
-              this.gyros[1] = gyroscope.y
-              this.gyros[2] = gyroscope.z
-              //console.log("Angular velocity along the X-axis " + gyroscope.x);
-              //console.log("Angular velocity along the Y-axis " + gyroscope.y);
-              //console.log("Angular velocity along the Z-axis " + gyroscope.z);
-              });
-              gyroscope.start();
-            });
-        });
-        */
-    }
+        }
+    },
 })
