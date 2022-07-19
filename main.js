b3fc7e9f6cc0310ac65bf65f112b4a5110ac7b2b
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-07-19 16:15:02 +0900

    Trying out different device snesor API since it doesn't work on iOS devices, plz work...

--- main.js
+++ main.js
@@ -12,50 +12,51 @@ const app = Vue.createApp({
         addInitialForm(initialForm) {
             this.experimentData.push(initialForm)
             this.eventNum++
+
+            navigator.permissions.query({ name: 'accelerometer' })
+            .then(result => {
+                if (result.state === 'denied') {
+                    console.log('Permission to use accelerometer sensor is denied.');
+                    return;
+                }
+                navigator.permissions.query({ name: 'accelerometer' })
+                .then(result => {
+                if (result.state === 'denied') {
+                    console.log('Permission to use accelerometer sensor is denied.');
+                    return;
+                }
+                    let acl = new Accelerometer({frequency: this.SENSORFREQ});
+                    acl.addEventListener('reading', () => {
+                    this.accels[0] = acl.x
+                    this.accels[1] = acl.y
+                    this.accels[2] = acl.z
+                    //console.log("Acceleration along the X-axis " + acl.x);
+                    //console.log("Acceleration along the Y-axis " + acl.y);
+                    //console.log("Acceleration along the Z-axis " + acl.z);
+                    });
+                    acl.start();
+                });
+                
+                navigator.permissions.query({ name: 'gyroscope' })
+                .then(result => {
+                  if (result.state === 'denied') {
+                    console.log('Permission to use gyroscope sensor is denied.');
+                    return;
+                  }
+                  let gyroscope = new Gyroscope({frequency: this.SENSORFREQ});
+                  gyroscope.addEventListener('reading', e => {
+                  this.gyros[0] = gyroscope.x
+                  this.gyros[1] = gyroscope.y
+                  this.gyros[2] = gyroscope.z
+                  //console.log("Angular velocity along the X-axis " + gyroscope.x);
+                  //console.log("Angular velocity along the Y-axis " + gyroscope.y);
+                  //console.log("Angular velocity along the Z-axis " + gyroscope.z);
+                  });
+                  gyroscope.start();
+                });
+            });
         },
     },
     beforeMount(){
-        navigator.permissions.query({ name: 'accelerometer' })
-        .then(result => {
-            if (result.state === 'denied') {
-                console.log('Permission to use accelerometer sensor is denied.');
-                return;
-            }
-            navigator.permissions.query({ name: 'accelerometer' })
-            .then(result => {
-            if (result.state === 'denied') {
-                console.log('Permission to use accelerometer sensor is denied.');
-                return;
-            }
-                let acl = new Accelerometer({frequency: this.SENSORFREQ});
-                acl.addEventListener('reading', () => {
-                this.accels[0] = acl.x
-                this.accels[1] = acl.y
-                this.accels[2] = acl.z
-                //console.log("Acceleration along the X-axis " + acl.x);
-                //console.log("Acceleration along the Y-axis " + acl.y);
-                //console.log("Acceleration along the Z-axis " + acl.z);
-                });
-                acl.start();
-            });
-            
-            navigator.permissions.query({ name: 'gyroscope' })
-            .then(result => {
-              if (result.state === 'denied') {
-                console.log('Permission to use gyroscope sensor is denied.');
-                return;
-              }
-              let gyroscope = new Gyroscope({frequency: this.SENSORFREQ});
-              gyroscope.addEventListener('reading', e => {
-              this.gyros[0] = gyroscope.x
-              this.gyros[1] = gyroscope.y
-              this.gyros[2] = gyroscope.z
-              //console.log("Angular velocity along the X-axis " + gyroscope.x);
-              //console.log("Angular velocity along the Y-axis " + gyroscope.y);
-              //console.log("Angular velocity along the Z-axis " + gyroscope.z);
-              });
-              gyroscope.start();
-            });
-        });
     }
 })
