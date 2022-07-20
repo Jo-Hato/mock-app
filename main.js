c40e654bb88f824f8d7551251d12f72b229c9c41
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-07-20 10:03:13 +0900

    Finishing aquiring device sensors.

--- main.js
+++ main.js
@@ -19,29 +19,33 @@ const app = Vue.createApp({
             updateFieldIfNotNull('Orientation_b', event.beta);
             updateFieldIfNotNull('Orientation_g', event.gamma);
         },
-        test(val, inc) {
-            val = inc;
-        },
         handleMotion(event) {
             /*updateFieldIfNotNull('Accelerometer_gx', event.accelerationIncludingGravity.x);
             updateFieldIfNotNull('Accelerometer_gy', event.accelerationIncludingGravity.y);
             updateFieldIfNotNull('Accelerometer_gz', event.accelerationIncludingGravity.z);
             updateFieldIfNotNull('Accelerometer_x', event.acceleration.x);
             updateFieldIfNotNull('Accelerometer_y', event.acceleration.y);
-            updateFieldIfNotNull('Accelerometer_z', event.acceleration.z);*/
-            
-            if (event.acceleration.x != null)
-                this.test(this.accels[0], event.acceleration.x)
-            if (event.acceleration.y != null)
-                this.accels[1] = event.acceleration.y;
-            if (event.acceleration.z != null)
-                this.accels[2] = event.acceleration.z;
-    
-            updateFieldIfNotNull('Accelerometer_i', event.interval, 2);
-    
+            updateFieldIfNotNull('Accelerometer_z', event.acceleration.z);
+
             updateFieldIfNotNull('Gyroscope_z', event.rotationRate.alpha);
             updateFieldIfNotNull('Gyroscope_x', event.rotationRate.beta);
             updateFieldIfNotNull('Gyroscope_y', event.rotationRate.gamma);
+            
+            updateFieldIfNotNull('Accelerometer_i', event.interval, 2);*/
+            
+            if (event.acceleration.x != null)
+                this.accels[0] = event.acceleration.x
+            if (event.acceleration.y != null)
+                this.accels[1] = event.acceleration.y
+            if (event.acceleration.z != null)
+                this.accels[2] = event.acceleration.z
+
+            if (event.rotationRate.alpha != null)
+                this.accels[0] = event.rotationRate.alpha
+            if (event.rotationRate.beta != null)
+                this.accels[1] = event.rotationRate.beta
+            if (event.rotationRate.gamma != null)
+                this.accels[2] = event.rotationRate.gamma
           }
     }
 })
