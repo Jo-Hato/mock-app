e2fae0acf1ba5c22b594f0675dd8fbc51b2775f5
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-07-20 09:36:28 +0900

    Miniscule changes, for debugging and integrating for iOS motion sensors...

--- main.js
+++ main.js
@@ -23,12 +23,16 @@ const app = Vue.createApp({
             updateFieldIfNotNull('Orientation_g', event.gamma);
         },
         handleMotion(event) {
-            //updateFieldIfNotNull('Accelerometer_gx', event.accelerationIncludingGravity.x);
-            //updateFieldIfNotNull('Accelerometer_gy', event.accelerationIncludingGravity.y);
-            //updateFieldIfNotNull('Accelerometer_gz', event.accelerationIncludingGravity.z);
-            this.updateIfNotNull(this.accels[0], event.acceleration.x)
-            updateIfNotNull(this.accels[1], event.acceleration.y)
-            updateIfNotNull(this.accels[2], event.acceleration.z)
+            updateFieldIfNotNull('Accelerometer_gx', event.accelerationIncludingGravity.x);
+            updateFieldIfNotNull('Accelerometer_gy', event.accelerationIncludingGravity.y);
+            updateFieldIfNotNull('Accelerometer_gz', event.accelerationIncludingGravity.z);
+            updateFieldIfNotNull('Accelerometer_x', event.acceleration.x);
+            updateFieldIfNotNull('Accelerometer_y', event.acceleration.y);
+            updateFieldIfNotNull('Accelerometer_z', event.acceleration.z);
+
+            //updateIfNotNull(this.accels[0], event.acceleration.x)
+            //updateIfNotNull(this.accels[1], event.acceleration.y)
+            //updateIfNotNull(this.accels[2], event.acceleration.z)
     
             updateFieldIfNotNull('Accelerometer_i', event.interval, 2);
     
