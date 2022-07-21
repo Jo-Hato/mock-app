1073d289bb4470b0b7786fbdea0ff30aeb554286
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-07-22 01:16:39 +0900

    The GroundTruth page is not working for the iOS, so tried to wrap the component with div to avoid rendering-defragmentation-error-thingy

--- components/GroundSurvey.js
+++ components/GroundSurvey.js
@@ -1,45 +1,46 @@
 app.component('ground-survey', {
   template:
   /*html*/
-  `<h1>Emotion Survey</h1>
-  <p>How do you feel?</p>
-  <p>1: low, 5: high</p>
+  `<div>
+    <h1>Emotion Survey</h1>
+    <p>How do you feel?</p>
+    <p>1: low, 5: high</p>
 
-  <div class="box">
-    <!-- Dyamn, should've used Vue's loop... -->
-    <label for="tired">Tired:</label>
-    <select id="tired" v-model.number="tired">
-      <option>5</option><option>4</option><option>3</option><option>2</option><option>1</option>
-    </select>
-    <p style="margin: 0; padding: 0; height: 0.5em;">&nbsp</p>
-    <label for="happy">Happy:</label>
-    <select id="happy" v-model.number="happy">
-      <option>5</option><option>4</option><option>3</option><option>2</option><option>1</option>
-    </select>
-    <p style="margin: 0; padding: 0; height: 0.5em;">&nbsp</p>
-    <label for="stress">Stress:</label>
-    <select id="stress" v-model.number="stress">
-      <option>5</option><option>4</option><option>3</option><option>2</option><option>1</option>
-    </select>
-    <p style="margin: 0; padding: 0; height: 0.5em;">&nbsp</p>
-    <label for="energy">Energy:</label>
-    <select id="energy" v-model.number="energy">
-      <option>5</option><option>4</option><option>3</option><option>2</option><option>1</option>
-    </select>
-    <p style="margin: 0; padding: 0; height: 0.5em;">&nbsp</p>
-    <label for="angry">Angry:</label>
-    <select id="angry" v-model.number="angry">
-      <option>5</option><option>4</option><option>3</option><option>2</option><option>1</option>
-    </select>
-    <p style="margin: 0; padding: 0; height: 0.5em;">&nbsp</p>
-    <label for="interested">Interested:</label>
-    <select id="interested" v-model.number="interested">
-      <option>5</option><option>4</option><option>3</option><option>2</option><option>1</option>
-    </select>
+    <div class="box">
+      <!-- Dyamn, should've used Vue's loop... -->
+      <label for="tired">Tired:</label>
+      <select id="tired" v-model.number="tired">
+        <option>5</option><option>4</option><option>3</option><option>2</option><option>1</option>
+      </select>
+      <p style="margin: 0; padding: 0; height: 0.5em;">&nbsp</p>
+      <label for="happy">Happy:</label>
+      <select id="happy" v-model.number="happy">
+        <option>5</option><option>4</option><option>3</option><option>2</option><option>1</option>
+      </select>
+      <p style="margin: 0; padding: 0; height: 0.5em;">&nbsp</p>
+      <label for="stress">Stress:</label>
+      <select id="stress" v-model.number="stress">
+        <option>5</option><option>4</option><option>3</option><option>2</option><option>1</option>
+      </select>
+      <p style="margin: 0; padding: 0; height: 0.5em;">&nbsp</p>
+      <label for="energy">Energy:</label>
+      <select id="energy" v-model.number="energy">
+        <option>5</option><option>4</option><option>3</option><option>2</option><option>1</option>
+      </select>
+      <p style="margin: 0; padding: 0; height: 0.5em;">&nbsp</p>
+      <label for="angry">Angry:</label>
+      <select id="angry" v-model.number="angry">
+        <option>5</option><option>4</option><option>3</option><option>2</option><option>1</option>
+      </select>
+      <p style="margin: 0; padding: 0; height: 0.5em;">&nbsp</p>
+      <label for="interested">Interested:</label>
+      <select id="interested" v-model.number="interested">
+        <option>5</option><option>4</option><option>3</option><option>2</option><option>1</option>
+      </select>
 
-    <button class="button"  @click="submitForm()">Submit</button>
-    <button class="button" v-if="debugMode" @click="skip()">Force Next</button>
-
+      <button class="button"  @click="submitForm()">Submit</button>
+      <button class="button" v-if="debugMode" @click="skip()">Force Next</button>
+    </div>
   </div>`,
   props: {
     debugMode: {
