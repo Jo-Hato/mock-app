2f20dc9c2379147d8bf6e3087f2cfd983d226770
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-07-26 17:14:49 +0900

    Experimenting with webSocket

--- components/EndScreen.js
+++ components/EndScreen.js
@@ -20,5 +20,31 @@ app.component('end-screen', {
         webSocket: null
       }
     },
-    
+    methods: {
+        created: function() {
+            console.log("Starting webSocket to WebSocket Server")
+            this.webSocket = new WebSocket("ws://172.24.55.112:8080")
+        
+            this.webSocket.onmessage = function(event) {
+                console.log(event);
+            }
+        
+            this.webSocket.onopen = function(event) {
+                console.log(event);
+                console.log("Successfully connected to the gmc websocket server...");
+            }
+
+            this.webSocket.onclose = function(event){
+                console.log(event);
+                console.log("Server Disconnected...");
+            };
+        },
+        sendMessage: function(message) {
+            console.log(this.webSocket);
+            this.webSocket.send(message);
+          }
+    },
+    beforeMount(){
+        this.created()
+    }
 })
