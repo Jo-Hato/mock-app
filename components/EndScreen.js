2b90a71b5c93e83ca335364f7402fc19cc6ed9d7
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-07-26 16:42:41 +0900

    Experimenting with webSocket

--- components/EndScreen.js
+++ components/EndScreen.js
@@ -2,10 +2,12 @@ app.component('end-screen', {
     template:
     /*html*/
     `<div>
-      <h1>The End of Data Collection</h1>
-      <p>Thank you for your participation! :D</p>
-      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-      <p>{{ runData }}</p>
+        <h1>The End of Data Collection</h1>
+        <p>Thank you for your participation! :D</p><br>
+
+        <button class="button" @click="sendMessage(JSON.stringify(runData))">Send Data</button>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <p>{{ runData }}</p>
     </div>`,
     props: {
         runData: {
@@ -15,8 +17,34 @@ app.component('end-screen', {
     },
     data() {
       return {
+        webSocket: null
       }
     },
     methods: {
+        created: function() {
+            console.log("Starting webSocket to WebSocket Server")
+            this.webSocket = new WebSocket("ws://172.24.55.112:8080")
+        
+            this.webSocket.onmessage = function(event) {
+                console.log(event);
+            }
+        
+            this.webSocket.onopen = function(event) {
+                console.log(event);
+                console.log("Successfully connected to the gmc websocket server...");
+            }
+
+            this.webSocket.onclose = function(event){
+                console.log(event);
+                console.log("Server Disconnected...");
+            };
+        },
+        sendMessage: function(message) {
+            console.log(this.webSocket);
+            this.webSocket.send(message);
+          }
+    },
+    beforeMount(){
+        this.created()
     }
-  })
+})
