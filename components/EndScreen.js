7f0530284bea0d2638994d164cca30f1c974c8dd
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-07-26 17:13:22 +0900

    Experimenting with webSocket

--- components/EndScreen.js
+++ components/EndScreen.js
@@ -5,7 +5,7 @@ app.component('end-screen', {
         <h1>The End of Data Collection</h1>
         <p>Thank you for your participation! :D</p><br>
 
-        <button class="button" @click="runData">Send Data</button>
+        
         <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
         <p>{{ runData }}</p>
     </div>`,
@@ -20,31 +20,5 @@ app.component('end-screen', {
         webSocket: null
       }
     },
-    methods: {
-        created: function() {
-            console.log("Starting webSocket to WebSocket Server")
-            this.webSocket = new WebSocket("ws://172.24.55.112:8080")
-        
-            this.webSocket.onmessage = function(event) {
-                console.log(event);
-            }
-        
-            this.webSocket.onopen = function(event) {
-                console.log(event);
-                console.log("Successfully connected to the gmc websocket server...");
-            }
-
-            this.webSocket.onclose = function(event){
-                console.log(event);
-                console.log("Server Disconnected...");
-            };
-        },
-        sendMessage: function(message) {
-            console.log(this.webSocket);
-            this.webSocket.send(message);
-          }
-    },
-    beforeMount(){
-        this.created()
-    }
+    
 })
