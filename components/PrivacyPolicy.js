4aaf2c63faf96096177e05596280f06f109ef1b2
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-07-28 10:06:55 +0900

    Added Privacy Policy and changed the initialSettings component

---
+++ components/PrivacyPolicy.js
@@ -0,0 +1,18 @@
+app.component('privacy-policy', {
+    template:
+    /*html*/
+    `<div>
+        <h1>Privacy Policy</h1>
+        <p>In this research, the following data will be collected:</p>
+        <div class="box">
+            <ul>
+                <li>Nickname/ID (Optional)</li>
+                <li>Age</li>
+                <li>Gender</li>
+                <li>Participant's interaction within this app (e.g. participant's respond to the forms, number of touches/deletions, device's motion sensor data)</li>
+            </ul>
+        </div>
+        <p>No personal-identifiable data will be collected, and the text inputs will not be recorded as keystrokes but only as total number of touches/deletions.</p>
+        <p>The data collected will be used for an upcoming research, which may be presented publically (e.g. a conference). Once the research is completed, one may request access by sending an E-mail to <i>jo.shimazaki@cps.akita-pu.ac.jp</i>.</p>
+    </div>`
+})
