d90a8b536f26f0196a933185de118f42ae655ce0
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-07-29 15:43:05 +0900

    Added some sort of fail-safe if websocket is not connected at the endScreen component

--- components/EndScreen.js
+++ components/EndScreen.js
@@ -4,14 +4,13 @@ app.component('end-screen', {
     `<div>
         <h1>The End of Data Collection</h1>
         <br>
-        <p>{{ message }}</p>
         <p v-if="internalStateNum == 0">Please press the button below to send the data.
             You may review the data collected, which is displayed at the bottom of this screen, before sending.
         </p>
         <br><p v-if="internalStateNum == 1">Thank you for your participation! :D<br>You may revert the keyboard settings, and close this app.</p>
 
-        <p>{{ message }}</p>
-        <button class="button" v-if="internalStateNum == 0" @click="sendMessage()">Send Data</button>
+        <button class="button" v-if="internalStateNum == 0" :disabled="!tried" @click="sendMessage()">Send Data</button>
+        <button class="button" v-if="isError" @click="created()">Connect Server</button>
         <br><br><br><br><br><br><br><br><br><br><br><br>
         <div v-if="internalStateNum == 0">
             <p>Collected Data:</p>
@@ -27,40 +26,44 @@ app.component('end-screen', {
     data() {
       return {
         webSocket: null,
-        message: "",
-        internalStateNum: 0
+        internalStateNum: 0,
+        isError: false,
+        tried: false
       }
     },
     methods: {
         created() {
+            this.tried = true
             console.log("Starting webSocket to WebSocket Server")
             this.webSocket = new WebSocket("wss://gmc.cps.akita-pu.ac.jp:8080")
             //this.webSocket = new WebSocket("ws://172.24.55.112:8080")
         
             this.webSocket.onmessage = function(event) {
                 console.log(event)
-                this.message += event
             }
         
             this.webSocket.onopen = function(event) {
                 console.log(event)
-                this.message += event
                 console.log("Successfully connected to the gmc websocket server...")
-                this.message += "Successfully connected to the gmc websocket server..."
             }
 
             this.webSocket.onclose = function(event){
                 console.log(event)
-                this.message += event
                 console.log("Server Disconnected...")
-                this.message += "Server Disconnected..."
             }
         },
         sendMessage() {
-            let message = JSON.stringify(this.runData)
             console.log(this.webSocket)
-            this.webSocket.send(message)
-            this.internalStateNum++
+            if (this.webSocket.readyState == 1) {
+                let message = JSON.stringify(this.runData)
+                console.log(this.webSocket)
+                this.webSocket.send(message)
+                this.internalStateNum++
+            } else {
+                alert("ERROR: Could not connect to the server.\nPlease press 'Connect Server' button.\n\nIf the issue consists, please notify the instructor.")
+                this.isError = true
+                this.tried = false
+            }
         }
     },
     beforeMount(){
--- components/PrivacyPolicy.js
+++ components/PrivacyPolicy.js
@@ -10,9 +10,10 @@ app.component('privacy-policy', {
                 <li>Age</li>
                 <li>Gender</li>
                 <li>Participant's interaction within this app (e.g. participant's respond to the forms, number of touches/deletions, device's motion sensor data)</li>
+                <li>Start/End time of this experiment</li>
             </ul>
         </div>
-        <p>No personal-identifiable data will be collected, and the text inputs will not be recorded as keystrokes but only as total number of touches/deletions.</p>
+        <p>No other data will be collected, and the text inputs will not be recorded as keystrokes but only as total number of touches/deletions.</p>
         <p>The data collected will be used for an upcoming research, which may be presented publically (e.g. research publication, conference), though the data collected will be anonymized. 
         Participant may send an E-mail to <i>jo.shimazaki@cps.akita-pu.ac.jp</i> in order to request access to this research, and will be notified when this research is completed.</p>
     </div>`
