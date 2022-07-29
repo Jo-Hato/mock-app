86383262b6262900adf7c96b0717982e81a7bc1d
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-07-29 14:18:54 +0900

    Overhauled the SCWT component to better match the referenced paper.

---
+++ WebSocket.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# WS server example that synchronizes state across clients
+
+import asyncio
+import json
+import logging
+import websockets
+import ssl
+
+from datetime import datetime
+
+logging.basicConfig()
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+
+# Generate with Lets Encrypt, copied to this location, chown to current user and 400 permissions
+ssl_cert = "/home/jo/ssl_pem/fullchain.pem"
+ssl_key = "/home/jo/ssl_pem/privkey.pem"
+
+ssl_context.load_cert_chain(ssl_cert, keyfile=ssl_key)
+
+fname = ""
+dir_name = "./MockApp_RunData/"
+
+async def accept(websocket, path):
+  while True:
+    data = await websocket.recv()
+    s = str(json.loads(data))
+    print("Received data: ", s)
+    fname = str(datetime.now())
+    fname = fname.replace(" ", "-")
+
+    with open(dir_name+fname+".json", "w") as file:
+      file.write(s)
+      print("Saved as: ", dir_name+fname)
+      file.close()
+
+      
+#start_server = websockets.serve(accept, "0.0.0.0", 8080, ssl=ssl_context)
+start_server = websockets.serve(accept, "140.227.201.242", 8080, ssl=ssl_context)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
--- components/Scwt.js
+++ components/Scwt.js
@@ -3,20 +3,24 @@ app.component('scwt', {
   /*html*/
   `<div>
     <h1>SCWT Excercise</h1>
-    <p v-if="internalStateNum == 0">Please choose the <u>the color of the font/written-word</u> above the colored boxes.<br> Press 'Start' when ready.</p><br>
-    <h2 style="color: red;" v-if="internalStateNum == 1">{{ sec }} second(s) left</h2>
-    <p>Current Score: {{ score }}/30</p>
+    <p v-if="internalStateNum == 0">In this excercise, you have to press the colored boxes maching the displayed messages which changes every 2 seconds.<br>
+      In the first phase, you have to choose the <u>font color</u>.<br><br>
+      <u>Please score more than 25 points</u><br>
+      Press 'Start' when ready.</p><br>
+    <p v-if="internalStateNum == 2">In this phase, you have to choose the <u>written word color</u>.<br></p>
+    <h2 style="color: red;" v-if="internalStateNum == 1 || internalStateNum == 3">{{ sec }} second(s) left</h2>
+    <p>Current Score: {{ score }}/25</p>
 
     <div class="box">
-      <p v-if="internalStateNum == 0" style="color: white;">Please choose the <u>written/font color</u>.</p>
-      <p v-if="internalStateNum == 1" style="color: white;">Please choose the <u>{{ writtenOrFont[rng] }} color</u>.</p><br>
+      <p v-if="internalStateNum == 0 && phase == 0" style="color: white;">Please choose the <u>font color</u>.</p>
+      <p v-if="internalStateNum == 1 || internalStateNum == 3" style="color: white;">Please choose the <u>{{ writtenOrFont[rng] }} color</u>.</p><br>
       <h2 style="text-shadow: 0 0 3px #FFFFFF, 0 0 5px #000000;" :class="word.class[word.color]">
         {{ word.written }}
       </h2>
       <div>
-        <input :disabled="internalStateNum == 0" v-model="picked" @click="onSubmit(color)" v-for="(color, index) in colors" :class="color" type="radio" :id="index" :value="color"/>
+        <input :disabled="internalStateNum == 0 || internalStateNum == 2" v-model="picked" @click="onSubmit(color)" v-for="(color, index) in colors" :class="color" type="radio" :id="index" :value="color"/>
       </div>
-      <button v-if="internalStateNum == 0" class="button" @click="startScwt()">Start</button>
+      <button v-if="internalStateNum == 0 || internalStateNum == 2" class="button" @click="startScwt()">Start</button>
       <button class="button" v-if="debugMode" @click="skip()" style="color: fuchsia;">Force Next</button>
     </div>
   </div>`,
@@ -32,61 +36,86 @@ app.component('scwt', {
       word: {"written": "", "color": null, class: ["rred", "ggreen", "bblue", "bblack", "yyellow"]},
       score: 0,
       picked: null,
-      writtenOrFont: ["written", "font"],
+      writtenOrFont: ["font", "written"],
       rng: null,
       internalStateNum: 0,
+      phase: 0,
       sec: 0,
       timer: null,
+      scrambleSec: 2,
+      scrambleTimer: null,
+      prevColor: null,
+      prevWord: null,
+      audio: new Audio('./assets/error.wav')
     }
   },
   methods: {
+    playSoundError() {
+      this.audio.loop = false
+      this.audio.play()
+    },
     startScwt(){
       this.rngWord()
       this.internalStateNum++
       //startTimer
       this.timer = setInterval(this.countDown, 1000)
+      this.scrambleTimer = setInterval(this.scrambleCountDown, 1000)
     },
     countDown() {
       this.sec--
       if (this.sec == 0){
         clearInterval(this.timer)
         this.internalStateNum++
+        this.sec = 60
+        this.phase = 1
+        clearInterval(this.scrambleTimer)
+        this.scrambleSec = 2
+        
+        if (this.internalStateNum == 4)
         this.skip()
       }
+    },
+    scrambleCountDown() {
+      if (this.scrambleSec == 1) {
+        this.rngWord()
+        return
+      }
+      this.scrambleSec--
     },
     rngWord(){
         this.word.written = this.colors[Math.floor(Math.random() * 5)]
         this.word.color = Math.floor(Math.random() * 5)
-        this.rng = Math.floor(Math.random() * 2)
+        if (this.phase == 0) {
+          this.rng = 0 //font
+        } else {
+          this.rng = 1 //word
+        }
+
+        //To avoid duplicate word-fontColor in a row
+        if (this.word.color == this.prevColor && this.word.written == this.prevWord) {
+          this.rngWord()
+        }
+        this.prevColor = this.word.color
+        this.prevWord = this.word.written
+        this.scrambleSec = 2
     },
     onSubmit(color) {
-      //console.log("COLOR ", color)
-      //console.log(this.word)
-      if (this.word.written == this.colors[this.word.color]) {
-        if (color == this.colors[this.word.written] || color == this.colors[this.word.color]) {
+      if (this.phase == 0) { //font
+        if (color == this.colors[this.word.color]) {
           this.score++
         } else {
           this.score--
+          this.playSoundError()
         }
-      } else {
-        if (this.rng == 1) { //Font color
-          if (color == this.colors[this.word.color]) {
-            this.score++
-          } else {
-            this.score--
-          }
-        } else { //Written color
-          if (color != this.colors[this.word.color]) {
-            this.score++
-          } else {
-            this.score--
-          }
+      } else { //word
+        if (color == this.word.written) {
+          this.score++
+        } else {
+          this.score--
+          this.playSoundError()
         }
       }
 
-      if (this.score == 30) {
-        this.skip()
-      }
       this.rngWord()
     },
     skip() {
